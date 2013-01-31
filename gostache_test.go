8d80efe2f8e598b9acd5e86436085c405a17734c
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2013-01-31 18:58:41 +0100

    reordered test-flow

--- gostache_test.go
+++ gostache_test.go
@@ -12,8 +12,9 @@ type Person struct {
 
 func TestRenderString(t *testing.T) {
 	p := Person{"Triny", 7}
-	expected := "Name: Triny\nAge: 7"
 	result := RenderString("Name: {{Name}}\nAge: {{Age}}", p)
+
+	expected := "Name: Triny\nAge: 7"
 	if result != expected {
 		t.Error("RenderString did not pass.")
 	}
@@ -21,8 +22,9 @@ func TestRenderString(t *testing.T) {
 
 func TestRenderFile(t *testing.T) {
 	p := Person{"Triny", 7}
-	expected := "Name: Triny\nAge: 7"
 	result := RenderFile("a", p)
+
+	expected := "Name: Triny\nAge: 7"
 	if !strings.Contains(result, expected) {
 		t.Error("RenderFile did not pass.")
 	}
@@ -30,8 +32,9 @@ func TestRenderFile(t *testing.T) {
 
 func TestRenderFileWithPartial(t *testing.T) {
 	p := Person{"Triny", 7}
-	expected := "Partial 7\n\nName: Triny\nAge: 7"
 	result := RenderFile("b", p)
+
+	expected := "Partial 7\n\nName: Triny\nAge: 7"
 	if !strings.Contains(result, expected) {
 		t.Error("RenderFile with a partial template did not pass.")
 	}
