012853931baf6978aa1d9342b662efc6523391f7
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2013-02-01 12:56:59 +0100

    restructured and commented some code

--- gostache.go
+++ gostache.go
@@ -12,15 +12,20 @@ import (
 )
 
 type Template struct {
-	template string
-	context  interface{}
+	Template string
+	Context  interface{}
 }
 
-func (t *Template) parseBlock(body string) (string, error) {
+func (t *Template) ParseSection(body string) (string, error) {
 	return body, nil
 }
 
-func (t *Template) parsePartial(body string) (string, error) {
+// ParsePartial will find the first occurence of the partial-mustache pattern
+// in body and replace it with the content of the partial template file that
+// matches the name used in the partial-mustache pattern if any such file exist
+// parsePartial assumes that partials as placed inside the
+// templates/partials/ directory in the current-working-directory
+func (t *Template) ParsePartial(body string) (string, error) {
 	r := regexp.MustCompile(`{{>(\w+)}}`)
 	match := r.FindStringSubmatch(body)
 	if len(match) > 0 {
@@ -44,12 +49,15 @@ func (t *Template) parsePartial(body string) (string, error) {
 	return body, nil
 }
 
-func (t *Template) parseString(body string) (string, error) {
+// ParseString will find the first occurence of the string-mustache pattern
+// and replace it with the string value of the field in t.Context that matches
+// the named defined in the string-mustache pattern
+func (t *Template) ParseString(body string) (string, error) {
 	r := regexp.MustCompile(`{{(\w+)}}`)
 	match := r.FindStringSubmatch(body)
 	if len(match) > 0 {
 		fieldname := match[1]
-		v := reflect.ValueOf(t.context)
+		v := reflect.ValueOf(t.Context)
 		value := v.FieldByName(fieldname)
 		str_value := fmt.Sprintf("%v", value.Interface())
 		body = strings.Replace(body, "{{"+fieldname+"}}", str_value, 1)
@@ -57,8 +65,13 @@ func (t *Template) parseString(body string) (string, error) {
 	return body, nil
 }
 
+// Render will loop over the content of t.Template as long as it can find the
+// mustache prefix `{{`
+// when it finds one, it will determine if its a section, partial or a string
+// -pattern and then tell either parseSection, parsePartial or parseString
+// to parse it
 func (t *Template) Render() (string, error) {
-	body := t.template
+	body := t.Template
 	err := errors.New("")
 	for {
 		index := strings.Index(body, "{{")
@@ -67,11 +80,11 @@ func (t *Template) Render() (string, error) {
 		}
 		switch {
 		case body[index+2:index+3] == "#" || body[index+2:index+3] == "^":
-			body, err = t.parseBlock(body)
+			body, err = t.ParseSection(body)
 		case body[index+2:index+3] == ">":
-			body, err = t.parsePartial(body)
+			body, err = t.ParsePartial(body)
 		default:
-			body, err = t.parseString(body)
+			body, err = t.ParseString(body)
 		}
 	}
 
@@ -82,6 +95,9 @@ func (t *Template) Render() (string, error) {
 	return body, nil
 }
 
+// RenderString will create a Template structure of the template and context
+// parameters and then ask Template.Render to render it. It then returns the
+// return value from Template.Render.
 func RenderString(template string, context interface{}) string {
 	tmpl := Template{template, context}
 	body, err := tmpl.Render()
@@ -91,6 +107,10 @@ func RenderString(template string, context interface{}) string {
 	return body
 }
 
+// RenderFile will look for a mustache-file in the templates directory of the
+// current-working-directory, and if it finds it, it reads its content and
+// passes that along with the context to RenderString. It will then take
+// the result from RenderString and return that.
 func RenderFile(filename string, context interface{}) string {
 	cwd := os.Getenv("CWD")
 	filepath := cwd + "templates/" + filename + ".mustache"
